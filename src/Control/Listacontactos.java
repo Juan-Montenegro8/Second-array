af471668efe0f8dfec4ee6271d3841006bf49e60
Juan <jj.monte2002@gmail.com>
2024-11-22 21:41:58 -0500

    Second commit

--- src/Control/Listacontactos.java
+++ src/Control/Listacontactos.java
@@ -1,42 +1,38 @@
 package Control;
 
 import Modelado.Persona;
-
-
-import java.util.Vector;
+import java.util.*;
 import javax.swing.JOptionPane;
 
 
 
 public class Listacontactos {
     
-    Vector<Persona> lis=new Vector<Persona>();
-    Persona pr;
+    List<Persona> lista=new ArrayList<>();
+    Persona pr = new Persona();;
     
     public void agregar(String nom,String ape, String id, String ed, String sex)
     {
-     
-        pr = new Persona();
         pr.setNombre(nom);
         pr.setApellido(ape);
         pr.setIdentificacion(id);
         pr.setEdad(ed);
         pr.setSexo(sex);
-        lis.add(pr);
+        lista.add(pr);
         JOptionPane.showMessageDialog(null, "Registrado");
         
     }
     
     public Persona buscar(String busc){
         int bus = Integer.parseInt(busc);
-        pr=lis.get(bus);
+        pr=lista.get(bus);
         return pr;
     }
     
     public void eliminar(String busc)
     {
         int bus = Integer.parseInt(busc);
-        lis.remove(bus);
+        lista.remove(bus);
         JOptionPane.showMessageDialog(null, "Eelimiando");
         
     }
@@ -50,14 +46,14 @@ public class Listacontactos {
         pr.setIdentificacion(id);
         pr.setEdad(ed);
         pr.setSexo(sex);
-        lis.set(bus,pr);
+        lista.set(bus,pr);
         JOptionPane.showMessageDialog(null, "Editado");
         
         
     }
     
     public void todob(){
-        lis.clear();
+        lista.clear();
     }
     
 }
